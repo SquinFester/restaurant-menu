667e884182b432f364510c4205fe17fc7d927727
SquinFester <bartoszprzymencki@gmail.com>
2023-03-10 19:53:41 +0100

    finished project with some help form a curs

--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import classes from "./HeaderCartButton.module.css";
 
@@ -6,14 +6,31 @@ import { FaShoppingCart } from "react-icons/fa";
 import CardContext from "../../../store/cart-context";
 
 const HeaderCartButton = ({ onClick }) => {
+  const [btnAnim, setBtnAnim] = useState(false);
+
   const cartCtx = useContext(CardContext);
 
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnAnim ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnAnim(true);
+
+    const timer = setTimeout(() => {
+      setBtnAnim(false);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [cartCtx.items]);
+
   return (
-    <button className={classes.cart} onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span>
         <FaShoppingCart />
       </span>
